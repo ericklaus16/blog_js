095d9a07c1816fc8378a18288bf69160a6a7dcf1
MichelC345 <michel.caesarr@gmail.com>
2023-12-01 21:38:22 -0300

    Adição da verificação de campos na criação de posts.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { pool } = require("./dbConfig"); //configuração feita em dbConfig para conectar-se ao postgre
 const cors = require('cors'); //permite que o front-end faça requisições a este back-end
-const bodyParser = require('body-parser'); //permite que o servidor manuseie dados enviados pelo front-end
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 8080;
 
@@ -10,18 +10,15 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
-    app.use(cors());
     next();
 })
-
-app.use(bodyParser.json()); //posts JSON
-app.use(bodyParser.urlencoded({ extended: false })); //posts normais
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT}: localhost:${PORT}`); //console imprime a porta em que o servidor está funcionando
 })
 
 app.get("/posts", async (req, res) => {
+    console.log("Retornando posts para aplicação...");
     const posts = await pool.query(`SELECT * FROM public.dadosblogjs`);
     return res.json(posts.rows);
 });
@@ -31,26 +28,28 @@ app.get(`/posts/:id`, async (req, res) => { //id = parâmetro 1
     return res.json(posts.rows[req.params.id - 1]);
 });
 
+app.use(bodyParser.json());
 app.post(`/create`, (req, res) => { //inserção de um novo post
     console.log(req.body);
-
     let {title, content} = req.body;
     let date = new Date; //data gerada no momento
     let tags = ["tag1", "tag2"]; //tags pré-definidas por enquanto
-    
-    if (!(!title || !content)) { //campos preenchidos (esta é a única verificação por enquanto)
+
+    //verificação básica de erros
+    if (!title || !content) {
+        res.status(500).send("Preencha todos os campos.");
+    }else if (title.length < 4) {
+        res.status(500).send("O título deve ter ao menos 4 caracteres.");
+    }else if (content.length < 10) {
+        res.status(500).send("O conteúdo do post deve ter ao menos 10 caracteres.");
+    }else {
         pool.query (
             `INSERT INTO public.dadosblogjs (title, content, date, tags)
             VALUES ($1, $2, $3, $4)
             RETURNING id`,
             [title, content, date, tags],
-            (err, res) => {
-                if (err) {
-                    throw err;
-                }
-            }
         );
-    }else {
-        throw Error('Preencha todos os campos.');
+        
+        res.sendStatus(200);
     }
 })
